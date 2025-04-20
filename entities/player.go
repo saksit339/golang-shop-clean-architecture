db39823f4acc3128b432845d7920d7f14a9ef625
saksit339 <112261890+saksit339@users.noreply.github.com>
2025-04-21 05:13:05 +0700

    Revert "add entity"
    This reverts commit 52e75fa7efcdf21818a26c911ba566c0acec77b9.

--- entities/player.go
+++ entities/player.go
@@ -1,13 +1,1 @@
 package entities
-
-import "time"
-
-type Player struct {
-	Id        string      `gorm:"primaryKey;type:varchar(64);"`
-	Email     string      `gorm:"type:varchar(128);unique;not null;"`
-	Name      string      `gorm:"type:varchar(128);not null;"`
-	Avatar    string      `gorm:"type:varchar(256);not null;default:'';"`
-	Inventory []Inventory `gorm:"foreignKey:PlayerId;references:Id;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	CreatedAt time.Time   `gorm:"autoCreateTime;not null;"`
-	UpdatedAt time.Time   `gorm:"autoUpdateTime;not null;"`
-}
--- entities/playerCoin.go
+++ entities/playerCoin.go
@@ -1,10 +1,1 @@
 package entities
-
-import "time"
-
-type PlayerCoin struct {
-	Id        uint64    `gorm:"primaryKey;autoIncrement;"`
-	PlayerId  string    `gorm:"type:varchar(64);not null;"`
-	Amount    int64     `gorm:"not null;"`
-	CreatedAt time.Time `gorm:"autoCreateTime;not null;"`
-}
